09ccb5a60633829bed571c49bd9c1a4c917de24a
Ray Bolaños <ray.bolanos@utec.edu.pe>
2025-10-21 18:22:37 -0500

    gabo

---
+++ src/main/java/com/example/oreohack/OreoHackApplication.java
@@ -0,0 +1,13 @@
+package com.example.oreohack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreoHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OreoHackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oreohack/OreoHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oreohack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OreoHackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
